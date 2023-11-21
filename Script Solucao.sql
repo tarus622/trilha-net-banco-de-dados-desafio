ca545b2200dee726ecd8d897c59c92e143382488
Tarus622 <davideoliveira622@gmail.com>
2023-11-21 10:38:25 -0300

    Solução do desafio

--- Script Solucao.sql
+++ Script Solucao.sql
@@ -1,10 +1,8 @@
--- 1 - Buscar o nome e ano dos filmes
 SELECT
 	Nome,
 	Ano
 FROM Filmes
 
--- 2 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
 SELECT
 	Nome,
 	Ano,
@@ -12,7 +10,6 @@ SELECT
 FROM Filmes
 ORDER BY Ano
 
--- 3 - Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a dura��o
 SELECT
 	Nome,
 	Ano,
@@ -20,7 +17,6 @@ SELECT
 FROM Filmes
 WHERE Nome = 'De Volta para o Futuro'
 
--- 4 - Buscar os filmes lan�ados em 1997
 SELECT
 	Nome,
 	Ano,
@@ -28,7 +24,6 @@ SELECT
 FROM Filmes
 WHERE Ano = 1997
 
--- 5 - Buscar os filmes lan�ados AP�S o ano 2000
 SELECT
 	Nome,
 	Ano,
@@ -36,8 +31,6 @@ SELECT
 FROM Filmes
 WHERE Ano > 2000
 
--- 6 - Buscar os filmes com a duracao maior que 100 e menor que 150,
--- ordenando pela duracao em ordem crescente
 SELECT
 	Nome,
 	Ano,
@@ -46,8 +39,6 @@ FROM Filmes
 WHERE Duracao > 100 AND Duracao < 150
 ORDER BY Duracao
 
--- 7 - Buscar a quantidade de filmes lan�adas no ano, agrupando por ano,
--- ordenando pela duracao em ordem decrescente
 SELECT
 	Ano,
 	COUNT(1) Quantidade
@@ -55,21 +46,17 @@ FROM Filmes
 GROUP BY Ano
 ORDER BY Quantidade DESC
 
--- 8 - Buscar os Atores do g�nero masculino, retornando o PrimeiroNome, UltimoNome
 SELECT
 	*
 FROM Atores
 WHERE Genero = 'M'
 
--- 9 - Buscar os Atores do g�nero feminino, retornando o PrimeiroNome, UltimoNome,
--- e ordenando pelo PrimeiroNome
 SELECT
 	*
 FROM Atores
 WHERE Genero = 'F'
 ORDER BY PrimeiroNome
 
--- 10 - Buscar o nome do filme e o g�nero
 SELECT
 	f.Nome,
 	g.Genero
@@ -77,7 +64,6 @@ FROM Filmes f
 INNER JOIN FilmesGenero fg ON f.Id = fg.IdFilme
 INNER JOIN Generos g ON fg.IdGenero = g.Id
 
--- 11 - Buscar o nome do filme e o g�nero do tipo "Mist�rio"
 SELECT
 	f.Nome,
 	g.Genero
@@ -86,7 +72,6 @@ INNER JOIN FilmesGenero fg ON f.Id = fg.IdFilme
 INNER JOIN Generos g ON fg.IdGenero = g.Id
 WHERE g.Genero = 'Mist�rio'
 
--- 12 - Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel
 SELECT
 	f.Nome,
 	a.PrimeiroNome,
